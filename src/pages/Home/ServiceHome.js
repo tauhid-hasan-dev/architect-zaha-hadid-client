fcab07cb28de83ffd824bc7e75d3b81a6ed224f3
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-08 17:05:30 +0300

    service card has been designed successfully

--- src/pages/Home/ServiceHome.js
+++ src/pages/Home/ServiceHome.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import ServiceCard from '../shared/ServiceCard/ServiceCard';
 
 const ServiceHome = () => {
@@ -17,11 +18,23 @@ const ServiceHome = () => {
     }, [])
 
     return (
-        <div>
-            {
-                services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
-            }
+        <div className='bg-nav-color px-28  py-20'>
+            <div className='flex justify-center pb-20'>
+                <p className='text-4xl text-white'>My Services</p>
+            </div>
+            <div className=' grid grid-cols-1  lg:grid-cols-3 gap-10 '>
+                {
+                    services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                }
+
+            </div>
+            <div className='flex justify-center  my-10'>
+                <Link to='/services'>
+                    <button className=" border py-3  px-10 text-white hidden lg:block hover:bg-slate-600">See All</button>
+                </Link>
+            </div>
         </div>
+
     );
 };
 
