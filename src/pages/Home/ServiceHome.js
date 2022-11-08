3dadf031549c041d2a4f552c8f42a458c547a1ad
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-08 16:32:48 +0300

    data uploaded for service successfully

--- src/pages/Home/ServiceHome.js
+++ src/pages/Home/ServiceHome.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ServiceCard from '../shared/ServiceCard/ServiceCard';
 
 const ServiceHome = () => {
+    const [services, setServices] = useState([]);
+
+    console.log(services);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/services', {
+            method: 'GET'
+        })
+            .then(res => res.json())
+            .then(data => {
+                setServices(data)
+            })
+    }, [])
+
     return (
         <div>
-            <p>this is service home</p>
+            {
+                services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+            }
         </div>
     );
 };
---
+++ src/pages/shared/ServiceCard/ServiceCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ServiceCard = ({ service }) => {
+    const { title, img, price, description } = service;
+    return (
+        <div>
+            <p>this is service card</p>
+        </div>
+    );
+};
+
+export default ServiceCard;
