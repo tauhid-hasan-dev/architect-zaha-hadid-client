7d7f682a4d5029db34251d2effbbb3a902f8862c
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-09 18:24:00 +0300

    url added from imagebb

--- src/pages/Other/ErrorElement.js
+++ src/pages/Other/ErrorElement.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const ErrorElement = () => {
     return (
-        <div className="bg-[url('https://images.unsplash.com/photo-1516557070061-c3d1653fa646?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')] bg-cover w-full object-cover h-full py-64 px-5 lg:py-72 lg:px-56  bg-opacity-20 relative cursor-pointer text-center">
+        <div className="bg-[url('https://i.ibb.co/FwfPTgL/Black-flag-svg.png')] bg-cover w-full object-cover h-full py-64 px-5 lg:py-72 lg:px-56  bg-opacity-20 relative cursor-pointer text-center">
             <div className='inset-0 bg-gray-800 mix-blend-overlay absolute bg-opacity-100 '></div>
             <div className='relative '>
                 <p className='text-white font-bold text-7xl lg:text-7xl mb-3 '>404!</p>
